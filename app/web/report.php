9da83273321934f18d165877171280a05185b4d1
Biswaranjan.Das@gmail.com <Biswaranjan.Das@gmail.com@c2cb1aca-443d-a9a7-1b8d-6cd3b16aecef>
2014-11-01 15:44:22 +0000

    Print Mode no wrap

---
+++ app/web/report.php
@@ -0,0 +1,115 @@
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="styles/print.css" media="print" />
+		<style>
+		* {
+		    font-family: Arial, "Times New Roman", Times, serif;
+		    font-size:12px;
+		}
+		table {
+		    border-collapse: collapse;
+		}
+		table, td, th {
+		    border: 1px solid black;
+		}
+		</style>
+	</head>
+	<body>
+	<div class="box">
+	  <div class="request" align="middle">
+		<?php
+			session_start();
+			if (isset($_SESSION['PID'])) {
+				include 'db.php';
+				$year=$_GET['year'];
+				$month=$_GET['mon'];
+				$days = date('t',mktime(0,0,0,$month,1,$year));
+
+
+				$dateStart=$year.'-'.$month.'-01';
+				$dateEnd=$year.'-'.$month.'-'.$days;
+
+				$Date = new DateTime($dateStart);
+
+				$reportQuery="select Question_Seq,Question_desc,Q_response,DAY(Dinalipi_date) as day from Question_master,Daily_transaction,Devotee ";
+				$reportQuery.="where Devotee.Devotee_id=Daily_transaction.Devotee_id and Question_master.Question_id=Daily_transaction.Question_id ";
+				$reportQuery.="and Devotee.Devotee_id='".$_SESSION['PID']."' ";
+				//$reportQuery.="and Devotee.Devotee_id='1' ";
+				$reportQuery.="and Dinalipi_date between '".$dateStart."' and '".$dateEnd."' order by Question_Seq,Dinalipi_date";
+
+				//echo $reportQuery;
+
+				$report_results = mysql_query($reportQuery);
+				$qid=-100;
+				$curdate=0;
+  	  			echo "<table cellspacing='0' cellpadding='1'>\n";
+  	  			echo "<tr><td style='text-align:center'><img height='100' src='/images/orgnz.gif'/></td>";
+  	  			echo "<td style='text-align:center' colspan='".($days)."'><span style='font-size:18px;color:#000099'>Nilachala Saraswata Sangha</span><br>";
+  	  			echo "<span style='color:#660033'>Branch: America Saraswata Sangha</span><br>";
+  	  			echo "<span style='font-size:20px;color:#FF3300'>DINALIPI</span>";
+  	  			echo "</td></tr>\n";
+  	  			echo "<tr><td style='text-align:center;font-weight: bold'>".$_SESSION['display_name']."</td>\n";
+  	  			echo "<td style='border:0px;text-align:center' colspan='".($days-10)."'>&nbsp;</td>\n";
+  	  			echo "<td style='border:0px;text-align:center' colspan='10'>".$Date->format('F,Y')."</td></tr>\n";
+				echo "<tr><td style='text-align:center;font-weight: bold'>Items</td>\n";
+				for ($counter=1;$counter<=$days;$counter++) {
+					$Date = new DateTime($year."-".$month."-".$counter);
+					echo "\n<td style='text-align:center;font-weight: bold' width='20px'>".$Date->format('d')."</td>";
+				}
+				echo "</tr>\n";
+				while($one_record = mysql_fetch_array($report_results)) {
+					if($qid==-100){
+						echo "<tr>";
+						echo "\n<td style='white-space: nowrap'>" .$one_record['Question_desc']. "</td>" ;
+						$day = $one_record['day'];
+						fill_gaps($curdate,$day);
+						echo "\n<td style='font-size:10px'>" .strtoupper($one_record['Q_response']). "</td>" ;
+					}else if($qid!=$one_record['Question_Seq']){
+						fill_gaps($curdate,$days);
+						echo "</tr>";
+						$curdate=0;
+						echo "<tr>";
+						echo "\n<td style='white-space: nowrap'>" .$one_record['Question_desc']. "</td>" ;
+						$day = $one_record['day'];
+						fill_gaps($curdate,$day);
+						echo "\n<td style='font-size:10px'>" .strtoupper($one_record['Q_response']). "</td>" ;
+					}else{
+						$day = $one_record['day'];
+						fill_gaps($curdate,$day);
+						echo "\n<td style='font-size:10px'>" .strtoupper($one_record['Q_response']). "</td>" ;
+					}
+					$qid=$one_record['Question_Seq'];
+					$curdate=$day;
+				}
+				echo "</tr>\n";
+				echo "<tr><td cellpadding='2px' style='border:0px;text-align:center' colspan='10'>Observations/Exceptions<br><br>&nbsp;</td>\n";
+				echo "<td cellpadding='2px' style='border:0px;text-align:left' colspan='".($days+1-20)."'>&nbsp;<br><br>President:</td>\n";
+				echo "<td cellpadding='2px' style='border-left:0px;border-bottom:0px;text-align:left' colspan='10'>Shree Shree Thakura Charanashrita<br><br>Signature:</td></tr>\n";
+				echo "<tr><td style='border:0px;' colspan='".($days+1)."'>&nbsp;</td></tr>";
+				echo "<tr><td style='border:0px;' colspan='".($days+1)."'>&nbsp;</td></tr>";
+				echo "<tr><td style='border:0px;' colspan='".($days+1)."'>&nbsp;</td></tr>";
+				echo "</table>";
+				//echo "<span class='print'><a href='index.php?mon=".$month."&year=".$year."'>Back to Calendar</a><br>";
+				//echo "<a href='#' onclick='javascript:window.print()'>Print</a></span>";
+
+			}else{
+  				echo "<a href='index.php'>[Session Expired] Visit Landing Page</a>";
+			}
+
+
+
+
+
+function fill_gaps($curdate,$day){
+	if($curdate<$day-1){
+		echo "\n<!--".$curdate."/".$day."-->\n" ;
+		for($ctr=$curdate+1;$ctr<$day;$ctr++){
+			echo "\n\t<td style='font-size:10px'>&nbsp;</td>" ;
+		}
+	}
+}
+		?>
+	  </div>
+</div>
+</body>
+</html>
