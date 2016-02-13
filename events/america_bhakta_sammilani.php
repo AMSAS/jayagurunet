22de85b9d1da16ec30897e6d6bb3002cd93f9fec
biswardas <biswaranjan.das@gmail.com>
2016-02-13 15:48:51 -0500

    Fix with header.php

--- events/america_bhakta_sammilani.php
+++ events/america_bhakta_sammilani.php
@@ -1,55 +1,38 @@
 <html>
+
 <head>
-	<meta content="text/html; charset=windows-1252" http-equiv="Content-Type" />
-	<meta content="Microsoft FrontPage 4.0" name="GENERATOR" />
-	<meta content="FrontPage.Editor.Document" name="ProgId" />
-	<title>America Saraswata Sangha</title>
-	<link href="../css/default.css" rel="stylesheet" type="text/css" /><script type="text/javascript" src="../script/maindev.js"></script><script type="text/javascript" src="../script/sidemenus.js"></script>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<meta name="GENERATOR" content="Microsoft FrontPage 4.0">
+<meta name="ProgId" content="FrontPage.Editor.Document">
+<title>America Saraswata Sangha</title>
+<link rel="stylesheet" type="text/css" href="../css/default.css" />
+<script type="text/javascript" src="../script/maindev.js"></script>
+<script type="text/javascript" src="../script/sidemenus.js"></script>
+
+
 </head>
-<body>
+<body >
 <div class="wrap">
 <div align="center">
-<table border="0" cellpadding="0" cellspacing="0" width="1004">
-	<tbody>
-		<tr>
-			<td height="20" width="1004">&nbsp;</td>
-		</tr>
-	</tbody>
-</table>
+<?php
+include '../header.php';
+?>
 
-<table border="0" cellpadding="0" cellspacing="0" class="tabletop" width="1004">
-	<tbody>
-		<tr>
-			<td align="left" background="../images/pglt.gif" height="2" valign="middle" width="2">&nbsp;</td>
-			<td align="left" background="../images/pght.gif" colspan="3" height="2" valign="middle" width="1000">&nbsp;</td>
-			<td align="left" background="../images/pgrt.gif" height="2" valign="middle" width="2">&nbsp;</td>
-		</tr>
-		<tr height="125">
-			<td align="left" background="../images/pgvl.gif" valign="middle" width="2">&nbsp;</td>
-			<td align="left" background="../images/logohd.gif" colspan="3" height="125" valign="middle" width="1000">&nbsp;</td>
-			<td align="left" background="../images/pgvr.gif" valign="middle" width="2">&nbsp;</td>
-		</tr>
-		<!-- title bar -->
-		<tr>
-			<td align="left" background="../images/pgvl.gif" height="30" valign="middle" width="2">&nbsp;</td>
-			<td align="center" background="../images/ttlbar.gif" colspan="3" height="30" valign="top" width="1000"><!-- menu --><script type="text/javascript">
-topmenu();
-</script><script type="text/javascript">
-cssdropdown.startchrome("chromemenu")
-</script><!-- menu --></td>
-			<td align="left" background="../images/pgvr.gif" height="30" valign="middle" width="2">&nbsp;</td>
-		</tr>
-		<!-- title bar -->
-	</tbody>
-</table>
+<table border="0" width="1004" cellpadding="0" cellspacing="0" bgcolor="#FFFFFF">
 
-<table bgcolor="#FFFFFF" border="0" cellpadding="0" cellspacing="0" width="1004">
-	<tbody>
-		<tr>
-			<td align="left" background="../images/pgvl.gif" valign="middle" width="2">&nbsp;</td>
-			<td class="traces" height="25" valign="top" width="275"><script type="text/javascript" src="../script/MPBackLinks.js"></script></td>
-			<td rowspan="3" valign="top" width="725"><!-- Body content -->
-			<table border="0" cellpadding="0" cellspacing="0" width="100%">
+  
+  
+  <tr>
+    <td align="left" valign="middle" width="2" background="../images/pgvl.gif">
+    </td>
+
+    <td width="275" height="25" valign="top" class="traces">
+		 <script type="text/javascript" src="../script/MPBackLinks.js"></script> 
+
+    </td>
+    <td width="725" valign="top" rowspan="3" >
+	  <!-- Body content -->
+      <table border="0" cellpadding="0" cellspacing="0" width="100%">
 				<tbody>
 					<tr>
 						<td align="center" width="100%"><!--<img src="../images/evntbanr.gif" width="725" height="160"/>--></td>
@@ -95,66 +78,95 @@ cssdropdown.startchrome("chromemenu")
 					</tr>
 				</tbody>
 			</table>
-			<!-- Body content ends --></td>
-			<td align="left" background="../images/pgvr.gif" valign="middle" width="2">&nbsp;</td>
-		</tr>
-		<tr>
-			<td align="left" background="../images/pgvl.gif" valign="middle" width="2">&nbsp;</td>
-			<td bgcolor="#ffffff" height="580" valign="top" width="275"><!--  Side Menu options start -->
-			<div class="sidemenu"><script type="text/javascript" >runAccordion(3);</script><!-- replace this index with folder index -->
-			<div class="AccordionContainer" id="AccordionContainer"><script type="text/javascript" >    defineMenu();  </script></div>
-			</div>
-			<!--  Side Menu options end --></td>
-			<td align="left" background="../images/pgvr.gif" valign="middle" width="2">&nbsp;</td>
-		</tr>
-		<tr>
-			<td align="left" background="../images/pgvl.gif" valign="middle" width="2">&nbsp;</td>
-			<td valign="top" width="275"><!--bgcolor="#c7fe94" >--><!-- Additional Page Information start -->
-			<table border="0" cellpadding="0" cellspacing="0" width="275">
-				<tbody>
-					<tr>
-						<td align="center" class="adtexts" height="30" valign="top" width="100%"><img height="18" src="../images/underline.gif" width="106" /></td>
-					</tr>
-					<tr>
-						<td class="adtexts" valign="top" width="100%">
-						<p>&quot;Bhakta Sammilani is a personification of evolution of collective feeling of the devotees. Here, &#39;bhaav&#39; (divine feeling) of one individual completes the &#39;a-bhaav&#39; (lack/deficit) of another. I wish, the spirits of universal brotherhood be established in the hearts of my disciples and devotees; and subsequently, find its expression in the whole of the human beings.&quot; - <b>Swami Nigamananda </b></p>
-						<img alt="America Sammilani 2013" height="180" src="./2013/images/grp2012.jpg" width="270" />
-						<h2>America Bhakta Sammilani</h2>
+	  <!-- Body content ends -->
+      </td>
 
-						<p>Grand celebration by all bhaktas reside in America.</p>
-						</td>
-					</tr>
-				</tbody>
-			</table>
-			<!-- Additional Page Information end --></td>
-			<td align="left" background="../images/pgvr.gif" valign="middle" width="2">&nbsp;</td>
-		</tr>
-	</tbody>
-</table>
-<!-- bottom row table -->
 
-<table bgcolor="#ffffff" border="0" cellpadding="0" cellspacing="0" width="1004">
-	<tbody>
-		<tr>
-			<td align="left" background="../images/pgvl.gif" height="29" valign="middle" width="2">&nbsp;</td>
-			<td align="center" background="../images/ftrbar.gif" height="29" valign="bottom" width="1000"><script type="text/javascript" >    copyRight();  </script></td>
-			<td align="left" background="../images/pgvr.gif" height="29" valign="middle" width="2">&nbsp;</td>
-		</tr>
-		<tr>
-			<td align="left" background="../images/pglb.gif" height="2" valign="middle" width="2">&nbsp;</td>
-			<td align="left" background="../images/pghb.gif" height="2" valign="middle" width="1000">&nbsp;</td>
-			<td align="left" background="../images/pgrb.gif" height="2" valign="middle" width="2">&nbsp;</td>
-		</tr>
-	</tbody>
-</table>
+  <td align="left" valign="middle" width="2" background="../images/pgvr.gif">
+    </td>
+  </tr>
+
+
+  
+  <tr>
+    <td align="left" valign="middle" width="2" background="../images/pgvl.gif">
+    </td>
+
+	   <td width="275" height="580" valign="top" bgcolor="#ffffff" >
+<!--  Side Menu options start -->	   
+		   
+<div class="sidemenu" >
+  <script type="text/javascript" >runAccordion(4);</script> <!-- replace this index with folder index -->
+<div id="AccordionContainer" class="AccordionContainer">
+  <script type="text/javascript" >    defineMenu();  </script>
+
 </div>
-<!-- center--></div>
-<!-- wrap -->
+</div>
 
+<!--  Side Menu options end -->	   
+      </td>
+
+
+    <td align="left" valign="middle" width="2" background="../images/pgvr.gif">
+    </td>
+  </tr>
+
+
+  
+  <tr>
+    <td align="left" valign="middle" width="2" background="../images/pgvl.gif">
+    </td>
+
+    <td width="275" valign="top" > <!--bgcolor="#c7fe94" >-->
+	  <!-- Additional Page Information start -->
+      <table border="0" width="275" cellpadding="0" cellspacing="0" >
+      <tr>
+          <td width="100%" height="30" align="center" valign="top" class="sidetexts" ><img src="../images/underline.gif" height="18" width="106"/>
+          </td>
+	  </tr>
+      <tr>
+	    <td width="100%" valign="top" class="sidetexts"><p>"Bhakta Sammilani is a personification of evolution of collective feeling of the devotees.  Here, 'bhaav' (divine feeling) of one individual completes the 'a-bhaav' (lack/deficit) of another. I wish, the spirits of universal brotherhood be established in the hearts of my disciples and devotees; and subsequently, find its expression in the whole of the human beings."  - <b>Swami Nigamananda </b></p>
+		  <img src="./2013/images/grp2012.jpg" width="270" height="180" alt="America Sammilani 2013"/><br/><h2>America Bhakta Sammilani</h2><p>Grand celebration by all bhaktas reside in America. </p>
+		</td>
+	  </tr>
+	</table>
+	  <!-- Additional Page Information end -->
+    </td>
+
+    <td align="left" valign="middle" width="2" background="../images/pgvr.gif">
+    </td> 
+	</tr>
+  
+</table>
+
+<!-- bottom row table -->
+<table border="0" width="1004" cellpadding="0" cellspacing="0" bgcolor="#ffffff">
+  <tr>
+      <td width="2" height="29" align="left" valign="middle" background="../images/pgvl.gif">
+    </td>
+    <td align="center" valign="bottom" width="1000" height="29" background="../images/ftrbar.gif" >
+    	  <script type="text/javascript" >    copyRight();  </script>
+    </td>
+    <td width="2" height="29" align="left" valign="middle" background="../images/pgvr.gif">
+    </td>
+  </tr>
+  <tr>
+      <td width="2" height="2" align="left" valign="middle" background="../images/pglb.gif">
+    </td>
+    <td align="left" valign="middle" width="1000" height="2" background="../images/pghb.gif">
+    </td>
+    <td width="2" height="2" align="left" valign="middle" background="../images/pgrb.gif">
+    </td>
+  </tr>
+</table>
+</div> <!-- center-->
+</div> <!-- wrap -->
 <div id="footer">
 <div align="center">
-<div class="footcenter"><script type="text/javascript" >    footCenter();  </script></div>
-<!-- footcenter--></div>
-<!-- center--></div>
-<!-- footer--></body>
+<div class="footcenter">
+	  <script type="text/javascript" >    footCenter();  </script>
+</div> <!-- footcenter-->
+</div> <!-- center-->
+</div> <!-- footer-->
+</body>
 </html>
